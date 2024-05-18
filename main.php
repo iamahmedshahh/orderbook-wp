5166a6b1b727aa1f0aad6a87c8f2791eb963dc6d
iamahmedshahh <artistahmed41@gmail.com>
2024-05-19 03:01:13 +0500

    testing changes

--- main.php
+++ main.php
@@ -16,13 +16,13 @@ function orderbook_admin_menu_page() {
         __('Orderbooks'),  // Page title
         __('Latest Orders'), // Menu title
         'manage_options',  // Capability required to access
-        'Orderbooks',   // Menu slug
+        'orderbooks-vue',   // Menu slug
         'orderbook_render_content', // Callback function to render content
         'dashicons-admin-plugins', // Icon URL or dashicon class
         10 // Menu position
     );
 }
-add_action('admin_menu', 'verus_vue_admin_menu_page');
+add_action('admin_menu', 'orderbook_admin_menu_page');
 
 function orderbook_render_content() {
     ?>
@@ -40,7 +40,7 @@ add_shortcode('order-book', 'orderbook_render_frontend'); // Short code usage: [
 
 
 function admin_enqueue_orderbook_scripts( $hook ) {
-    if ( 'toplevel_page_verus-vue' === $hook ) {
+    if ( 'toplevel_page_orderbooks-vue' === $hook ) {
         wp_enqueue_script('app-script', plugins_url('/dist/js/app.a58f637f.js', __FILE__), array(), null, true);
         wp_enqueue_style('app-style', plugins_url('/dist/css/app.7f5d4a7b.css', __FILE__));
         error_log($hook); // For testing (to be removed)
