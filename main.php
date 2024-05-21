37483cbda460dd8585d1386d4d8825a803fc397c
iamahmedshahh <artistahmed41@gmail.com>
2024-05-22 01:53:40 +0500

    updates

--- main.php
+++ main.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-Plugin Name: Komodefi Markets Plugin
+Plugin Name: Komodefi Markets
 Description: Live Orders from AtomicDex, Shortcode [order-book].
 Version: 1.0
 Author: Ahmed Shah
@@ -35,26 +35,30 @@ Author: Ahmed Shah
     // <?php */
 // }
 
+
+// function admin_enqueue_orderbook_scripts( $hook ) {
+//     if ( 'toplevel_page_orderbook-vue' === $hook ) {
+//         wp_enqueue_script('app-script', plugins_url('/dist/js/app.ae2d6dc5.js', __FILE__), array('jquery'), '1.0.0', true);
+//         wp_enqueue_script('vendors-script', plugins_url('dist/js/chunk-vendors.2dead338.js', __FILE__), array('jquery'), '1.0.0', true);
+//         wp_enqueue_style('app-style', plugins_url('/dist/css/app.cccafb64.css', __FILE__), array(), '1.0.0');
+//         if ( defined('WP_DEBUG') && WP_DEBUG ) {
+//             error_log($hook); // For testing (to be removed in production)
+//         }
+//     }
+// }
+// add_action('admin_enqueue_scripts', 'admin_enqueue_orderbook_scripts');
+
+
 function orderbook_render_frontend() {
     return '<div id="plugin-orderbook"></div>';
 }
 add_shortcode('order-book', 'orderbook_render_frontend'); // Shortcode usage: [order-book]
 
-function admin_enqueue_orderbook_scripts( $hook ) {
-    if ( 'toplevel_page_orderbook-vue' === $hook ) {
-        wp_enqueue_script('app-script', plugins_url('/dist/js/app.ae2d6dc5.js', __FILE__), array('jquery'), '1.0.0', true);
-        wp_enqueue_script('vendors-script', plugins_url('dist/js/chunk-vendors.2dead338.js', __FILE__), array('jquery'), '1.0.0', true);
-        wp_enqueue_style('app-style', plugins_url('/dist/css/app.cccafb64.css', __FILE__), array(), '1.0.0');
-        if ( defined('WP_DEBUG') && WP_DEBUG ) {
-            error_log($hook); // For testing (to be removed in production)
-        }
-    }
-}
-add_action('admin_enqueue_scripts', 'admin_enqueue_orderbook_scripts');
-
 function front_enqueue_orderbook_scripts() {
-    wp_enqueue_script('app-script', plugins_url('/dist/js/app.ae2d6dc5.js', __FILE__), array('jquery'), '1.0.0', true);
-    wp_enqueue_script('vendors-script', plugins_url('dist/js/chunk-vendors.2dead338.js', __FILE__), array('jquery'), '1.0.0', true);
-    wp_enqueue_style('app-style', plugins_url('/dist/css/app.cccafb64.css', __FILE__), array(), '1.0.0');
+    if (is_singular() && has_shortcode(get_post()->post_content, 'order-book')) {
+        wp_enqueue_script('app-script', plugins_url('/dist/js/app.ae2d6dc5.js', __FILE__), array('jquery'), '1.0.0', true);
+        wp_enqueue_style('app-style', plugins_url('/dist/css/app.cccafb64.css', __FILE__), array(), '1.0.0');
+        wp_enqueue_script('vendors-script', plugins_url('dist/js/chunk-vendors.2dead338.js', __FILE__), array('jquery'), '1.0.0', true);
+    }
 }
 add_action('wp_enqueue_scripts', 'front_enqueue_orderbook_scripts');
---
+++ readme.txt
@@ -0,0 +1,13 @@
+=== Komodefi Markets ===
+Contributors: Ahmed Shah
+Tags: orders, atomicdex, live
+Requires at least: 5.0
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Live Orders from AtomicDex.
+
+== Description ==
+
+This plugin displays live Orders against any pair from AtomicDex.
